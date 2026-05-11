6267e0586384562bd8d7f5eeb6c698f0227d3932
Deepanshuguptagpt <deepanshu.gupta1817@gmail.com>
2026-05-11 13:00:36 +0530

    Nested fields

---
+++ pydantic tutorial/nested_models.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+
+class Address(BaseModel):
+    street: str
+    city: str
+    state: str
+    zip_code: str
+
+class Patient(BaseModel):   
+    name: str
+    age: int
+    gender : str
+    address: Address
+
+address_dict = {'street':'123 Main St','city':'New York','state':'NY','zip_code':'10001'}
+address = Address(**address_dict)
+
+patient_info = {'name':'Deepak','age':30,'gender':'male','address':address}
+patient = Patient(**patient_info)
+
+print(patient)
